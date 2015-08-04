e41d987baf600c69536007556e2646b1b0977176
gaspardpetit <gaspardpetit@gmail.com>
2015-08-04 00:45:29 -0400

    Modified config file for visual studio 2015

--- config.h
+++ config.h
@@ -44,7 +44,7 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
+/* #undef HAVE_UNISTD_H */
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
